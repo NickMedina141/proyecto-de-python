22ffc23c60904b1339de4d74875a26c2871213a7
NickMedina141 <129890087+NickMedina141@users.noreply.github.com>
2023-04-08 12:12:56 -0500

    Update borrador 3.py

--- borrador 3.py
+++ borrador 3.py
@@ -19,35 +19,55 @@ def sistema():
     | 1. Docente             |
     | 2. Estudiante          |
     |________________________|''')
-        
-        rol_usuario = int(input("\n¿Señor usuario que rol es usted?: "))
+        while True:
+            try:
+                rol_usuario = int(input("\n¿Señor usuario que rol es usted?: "))
+            except:
+                print("Error")
+                continue
+            if rol_usuario <= 0 or rol_usuario > 2:
+                print("Error")
+            else:
+                break
         if rol_usuario == 1:
             docente(database,comprobante)
         elif rol_usuario == 2:
-            estudiante(database,comprobante)
+            estudiante(database,comprobante,)
             
-def docente(database,comprobante): 
+def docente(database,comprobante):
     while True:
         while True:
             try:
-                codigoV = int(input("\nSeñor docente por favor ingrese su codigo de identificacion: "))
+                codigoVdocente = int(input("\nSeñor docente por favor ingrese su codigo de identificacion: "))
             except: 
                 print("Error")
                 continue
-            if codigoV < 0:
-                print("Error, no se aceptan numeros negativos")
+            if codigoVdocente < 0:
+                print("Error")
             else:
                 break
-        for i in database[4]:
-            if codigoV == i:
+            
+        for j in database[5].keys():
+            if codigoVdocente == j:
                 comprobante = True
                 break
         if (comprobante):
-            print(f"\nBienvenido de vuelva Docente {database[4][codigoV]}")
+            print(f"\nLo lamentamos docente pero su codigo es identico al del estudiante {database[5][j]}")
+            break
+        
+        for i in database[4]:
+            if codigoVdocente == i:
+                comprobante = True
+                break
+        if (comprobante):
+            print(f"\nBienvenido de vuelva Docente {database[4][codigoVdocente]}")
+            
         else:
             nombreDocente = str(input("\nSeñor docente por favor ingrese su nombre: "))
-            database[4][codigoV] = nombreDocente 
-            print(database) # quitar cuando se termine por completo el codigo
+            if nombreDocente.isnumeric():
+                print("Error")
+            else:
+                database[4][codigoVdocente] = nombreDocente 
         print('''
     ________________________________________
     |           Menu de opciones           |
@@ -58,52 +78,82 @@ def docente(database,comprobante):
     | 4. salir                             |
     |______________________________________|''')       
         
-        opcion = int(input("\nEliga una opcion: "))
+        while True:
+            try:
+                opcion = int(input("\nEliga una opcion: "))
+            except:
+                print("Error")
+                continue
+            if opcion <= 0 or opcion > 4:
+                print("Error")
+            else:
+                break
+            
         if opcion == 1:
             creacionQuiz(database,comprobante)
-        elif opcion == 2: 
+        elif opcion == 2:
             cantidad2 = len(database[1])
             if cantidad2 > 0:
+                print("\nRespuestas del quiz:\n")
                 for i, j in database[1].items():
-                    print(f"Pregunta {i}: Respuesta: {j}")
+                    print(f"Pregunta {i}: ¿{database[0][i]}?\tRespuesta: {j}")
+                sistema()
             else:
                 print("No hay respuestas en la base de datos del quiz")
-        elif opcion == 3: 
+        elif opcion == 3:
             cantidad3 = len(database[3])
             if cantidad3 > 0:
-                print("--Notas de estudiantes--")
+                print("\n--Notas de estudiantes--\n")
                 for k, s in database[3].items():
-                    print(f"\n{database[5][k]} Nota final: {s}")
-                    
-                print(f"La nota mas alta fue del estudiante {database[5][k]} con su nota de {max(database[3].values())} ")
-                print(f"La nota mas baja fue del estudiante {database[5][k]} con su nota de {min(database[3].values())} ")
-                # hacer que muestre el nombre del estudiante y su nota, ademas
-                # si es posible hacer promedio cual es el mayor puntaje y el menor
+                    print(f"{database[5][k]}: {s}")
+                Position1 = min(database[3]);Position2 = max(database[3])  
+                print(f"\nLa nota mas alta fue del estudiante {database[5][Position1]} con {max(database[3].values())} ")
+                print(f"La nota mas baja fue del estudiante {database[5][Position2]} con {min(database[3].values())} ")
             else:
                 print("No hay notas alguna al cual mirar")
-        elif opcion == 4: # terminado
+        elif opcion == 4:
             for i in reversed(range(1,6)):
                 print(f"__Cerrando sistema en {i}__")
                 time.sleep(1)
             print("\nSistema cerrado")
             break
-            
-def estudiante(database,comprobante): # terminado pero mirar si se puede pulir
+    
+def estudiante(database,comprobante):
     while True:
-        codigoV = int(input("\nSeñor estudiante por favor ingrese su codigo de identificacion: "))
-        for i in database[5]:
-            if codigoV == i:
+        while True:
+            try:
+                codigoVestudiante = int(input("\nSeñor estudiante por favor ingrese su codigo de identificacion: "))
+            except:
+                print("Error")
+                continue
+            if codigoVestudiante < 0: 
+                print("Error")
+            else:
+                break
+        
+        for i in database[4].keys(): #docente
+            if codigoVestudiante == i:
                 comprobante = True
                 break
         if (comprobante):
-            print(f"\nBienvenido de vuelva {database[5][codigoV]}")
+            print(f"\nLo lamentamos estudiante pero su codigo es identico al del docente {database[4][i]}")
+            break
+        
+        for j in database[5]:
+            if codigoVestudiante == j:
+                comprobante = True
+                break
+        if (comprobante):
+            print(f"\nBienvenido de vuelva {database[5][codigoVestudiante]}")
         else:
             nombreEstudiante = str(input("\nSeñor estudiante por favor ingrese su nombre: "))
-            database[5][codigoV] = nombreEstudiante    
-            print(database) # quitar cuando se termine por completo el codigo
-        quiz(database,comprobante,codigoV)
-            
-def creacionQuiz(database,comprobante): # creacion con quiz hay fallo
+            if nombreEstudiante.isnumeric():
+                print("Error")
+            else:
+                database[5][codigoVestudiante] = nombreEstudiante    
+        quiz(database,comprobante,codigoVestudiante)
+    
+def creacionQuiz(database,comprobante): 
     while True:
         print('''
     ________________________________________
@@ -114,23 +164,43 @@ def creacionQuiz(database,comprobante): # creacion con quiz hay fallo
     |______________________________________|   
             ''')
         opciones = int(input("\nEliga una opcion: "))
-        # opcion 1 de agregar
-        if opciones == 1: 
-            preguntas = int(input("\n¿Cuantas preguntas tendra el quiz?: "))
+        
+        if opciones == 1:
+            while True:
+                try: 
+                    preguntas = int(input("\n¿Cuantas preguntas tendra el quiz?: "))
+                except:
+                    print("Error")
+                    continue
+                if preguntas <= 0:
+                    print("Error")
+                else:
+                    break
+                
             for i in range(preguntas):
-                pregunta = input(f"\nEscriba la pregunta {i+1}: ")
-                database[0][i+1] = pregunta
+                pregunta = input(f"\nEscriba la pregunta {i+1}: ")    
                 respuesta = input(f"\nIngrese la respuesta de la pregunta {i+1}: ")
-                porcentaje = 5.0 / preguntas;database[2][i+1] = porcentaje
-                database[1][i+1] = respuesta
+                database[0][i+1] = pregunta
+                porcentaje = 5.0 / preguntas
                 database[2][i+1] = porcentaje
+                database[1][i+1] = respuesta
                 print(database)
             sistema() 
-            # opcion 2 de modificar    
+
         elif opciones == 2:
             cantidad = len(database[0])
             if cantidad > 0:
-                pregunta_modficiar = int(input("\nIngrese la pregunta que desea modificar: "))
+                while True:
+                    try:
+                        pregunta_modficiar = int(input("\nIngrese la pregunta que desea modificar: "))
+                    except:
+                        print("Error")
+                        continue
+                    if pregunta_modficiar <= 0:
+                        print("Error")
+                    else:
+                        break
+                    
                 for i in database[0].keys():
                     if pregunta_modficiar == i:
                         comprobante = True
@@ -142,15 +212,15 @@ def creacionQuiz(database,comprobante): # creacion con quiz hay fallo
                 else:
                     print("No se encontro la pregunta que deseaba modificar")
             else:
-                print("Lo lamentamos no hay ninguna pregunta en la base de datos del quiz al cual modificar")
+                print("Lo lamentamos pero no hay ninguna pregunta en la base de datos del quiz al cual modificar")
             
-def quiz(database,comprobante,codigoV):
+def quiz(database,comprobante,codigoVestudiante):
     cantidad = len(database)
     if cantidad > 0:
-        print("\nQuiz\n")
+        print("\n-------Quiz-------\n")
         notaFinal = 0
         for i, j in database[0].items():
-            print(f"\n{i}. {j}")
+            print(f"\n{i}. ¿{j}?")
             respuesta = input(f"\n¿La respuesta de la pregunta {i} es?: ")
             for k in database[1].values():
                 if respuesta == k:
@@ -162,13 +232,24 @@ def quiz(database,comprobante,codigoV):
                         else:
                             pass
                     break
-        codigoEstudiante = codigoV
+        codigoEstudiante = codigoVestudiante
         database[3][codigoEstudiante] = notaFinal 
         print(f"La nota final del estudiante es: {notaFinal}")
+        
+        '''
+        pregunta = str(input("¿Desea volver a intentar el examen si/no?: ")).lower()
+        if pregunta == "si":
+            for i in reversed(range(1,11)):
+                print(f"Podra hacer el quiz en {i}")
+            input("Listo su quiz ya esta listo, dele enter para comenzar")
+            quiz()
+        else:
+            break
+        '''
         sistema()            
     else:
         print("Lo lamentamos no hay ninguna pregunta en la base de datos del quiz ")
         
 if __name__ == "__main__":
-    print("Bienvenidos al sistema\n")
+    print("     Bienvenidos al sistema")
     sistema()
